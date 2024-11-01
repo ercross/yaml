68c4738dc628d3a37a2e9f0f229b8330936b4530
ercross <tobins4u@gmail.com>
2024-11-01 20:30:47 +0100

    test for scalar node using token trie

---
+++ parser/token_trie_test.go
@@ -0,0 +1,21 @@
+package parser
+
+import (
+	"github.com/ercross/yaml"
+	"github.com/ercross/yaml/test/data"
+	"testing"
+)
+
+func TestScalarNodeFinder(t *testing.T) {
+	initTokenTrie()
+
+	finder := newNodeTypeFinder()
+
+	for _, sampleScalar := range data.ExpectedScalarTokens {
+		finder.findMatch(sampleScalar)
+		if finder.done && finder.nodeType() != yaml.NodeTypeScalar {
+			t.Errorf("expected node type %d, got %d", yaml.NodeTypeScalar, finder.nodeType())
+		}
+	}
+
+}
